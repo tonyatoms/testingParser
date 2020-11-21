86033d3f874dab861ea8c082b785d35a7e7df1be
Tony Adams <tadams@mediafly.com>
2020-11-21 10:05:08 -0600

    initial files

---
+++ TestingParser/TestingParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingParserApp.swift
+//  TestingParser
+//
+//  Created by Tony Adams on 11/21/20.
+//
+
+import SwiftUI
+
+@main
+struct TestingParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
